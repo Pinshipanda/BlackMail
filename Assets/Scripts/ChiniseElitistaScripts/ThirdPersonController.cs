1cd26237f377301b8d2b9e22e9704bab57560d74
Cguy6706 <57649157+Cguy6706@users.noreply.github.com>
2020-01-18 01:39:51 -0600

    ThirdPersonControllerV1

---
+++ Assets/Scripts/ChiniseElitistaScripts/ThirdPersonController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public float InpuX = 0, InpuY = 0;
+    public float VelMov = 1;
+    public float turnSpeed = 1.0f;
+    public Transform camara;
+
+    Rigidbody rb;
+    Vector3 direccion = Vector3.zero, direccionFinal = Vector3.zero;
+    Quaternion targetRotation;
+    Collider Col;
+
+    //public Animator ACon;
+    int animState = 0;
+    float speed = 0, walkSpeed, RunSpeed;
+    bool isRunning = false, isalive = true;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Col = GetComponent<Collider>();
+        walkSpeed = VelMov;
+        RunSpeed = VelMov * 2.0f;
+        if (rb == null)
+        {
+            Debug.LogError("No hay rigibody BOOOOOOOOOOI");
+            enabled = false;
+        }
+
+        if (Col == null)
+        {
+            Debug.LogError("No hay Collider BOOOOOOOOOOI");
+            enabled = false;
+        }
+        targetRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (isalive)
+        {
+            InpuX = Input.GetAxis("Horizontal");
+            InpuY = Input.GetAxis("Vertical");
+            isRunning = Input.GetKey(KeyCode.LeftShift);
+            direccion.x = InpuX;
+            direccion.z = InpuY;
+
+            MovementBehaviour();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition((transform.position + (direccionFinal * VelMov) * Time.fixedDeltaTime));
+    }
+
+    void MovementBehaviour()
+    {
+        if (direccion.sqrMagnitude > 0.01f)
+        {
+            animState = 1;
+            VelMov = walkSpeed;
+            if (isRunning)
+            {
+                animState = 2;
+                VelMov = RunSpeed;
+            }
+
+            //ACon.SetInteger("MoveState", animState);
+
+            direccionFinal = camara.TransformDirection(direccion);
+            direccionFinal.Set(direccionFinal.x, 0, direccionFinal.z);
+
+            if (direccionFinal.sqrMagnitude > 1.0f)
+                direccionFinal.Normalize();
+
+            speed = direccionFinal.sqrMagnitude;
+            //if (speed < 0.1f) speed = 0.1f;
+            //ACon.SetFloat("Speed", speed);
+            targetRotation = Quaternion.LookRotation(direccionFinal);
+        }
+        else
+        {
+            direccionFinal = Vector3.zero;
+            animState = 0;
+            //ACon.SetInteger("MoveState", animState);
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+}
