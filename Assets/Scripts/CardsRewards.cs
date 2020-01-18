b3506261d1ce20108dd080766f13013011f5f3e4
MarioGz <marioalberto2306@gmail.com>
2020-01-18 03:37:01 -0600

    jeje

---
+++ Assets/Scripts/CardsRewards.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System
+
+public class CardsRewards : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> WhiteMailSoldList;
+    private List<GameObject> BlackMailSoldList;
+
+    Gamemanager gamemanager;
+
+    private void Start()
+    {
+        WhiteMailSoldList = new List<GameObject>();
+        BlackMailSoldList = new List<GameObject>();
+
+        gamemanager = GameObject.Find("GameManager").GetComponent<Gamemanager>();
+    }
+
+    private void Update()
+    {
+        //var x = gamemanager.GetTotalWhiteMail();
+
+        
+
+        for(int i = 0; i < Whitemail; i++ )
+        {
+
+        }
+    }
+}
