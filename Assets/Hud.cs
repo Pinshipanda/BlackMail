d2fd8f9387f96ae68f2d0cc485332b3d2927437d
uterbraten147 <147and@gmail.com>
2020-01-19 12:15:46 -0600

    Ultimos cambios

--- Assets/Hud.cs
+++ Assets/Hud.cs
@@ -6,12 +6,21 @@ public class Hud : MonoBehaviour
 {
     Gamemanager gamemanager;
     public Image img;
-
+    float t=0;
     void Start()
     {
         StartCoroutine(getManager());
     }
 
+    void Update()
+    {
+        if (gamemanager)
+        {
+            t=gamemanager.counter;
+            t = MathCH.Remap(t, 0, 300, 0, 1);
+            img.fillAmount = 1-t;
+        }
+    }
     IEnumerator getManager()
     {
         yield return new WaitForSeconds(1.0f);
