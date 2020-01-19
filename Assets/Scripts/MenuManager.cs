6dc7b582e8346b2829fc77026b07ed0367c9efb2
uterbraten147 <147and@gmail.com>
2020-01-19 06:17:42 -0600

    f

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -28,11 +28,7 @@ public class MenuManager : MonoBehaviour
         menuCanvas.SetActive(false);
         sliderCanvas.SetActive(true);
 
-        // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
-
-        // Smoothly move the camera towards that target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+     
 
     }
 
