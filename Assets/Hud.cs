bdca2fb0314783297e961146427176ab0e8c7fac
uterbraten147 <147and@gmail.com>
2020-01-19 12:02:52 -0600

    laTusa

---
+++ Assets/Hud.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Hud : MonoBehaviour
+{
+    Gamemanager gamemanager;
+    public Image img;
+
+    void Start()
+    {
+        StartCoroutine(getManager());
+    }
+
+    IEnumerator getManager()
+    {
+        yield return new WaitForSeconds(1.0f);
+        gamemanager = FindObjectOfType<Gamemanager>();
+    }
+}
